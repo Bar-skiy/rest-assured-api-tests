48a2aace2b87e3239348103ca28ab2e408867e9a
Bar-skiy <110903517+Bar-skiy@users.noreply.github.com>
2025-07-20 20:39:22 +0200

    Добавил автотесты и настроил .gitignore

--- src/test/java/api/BaseTest.java
+++ src/test/java/api/BaseTest.java
@@ -1,11 +1,20 @@
 package api;
 
 import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
 
 public class BaseTest {
+
+    protected static RequestSpecification requestSpec;
+
     @BeforeAll
     public static void setup() {
         RestAssured.baseURI = "https://reqres.in/api";
+        requestSpec = RestAssured.given();
+        requestSpec = RestAssured
+                .given()
+                .header("x-api-key", "reqres-free-v1");
+
     }
 }
--- src/test/java/api/UserTests.java
+++ src/test/java/api/UserTests.java
@@ -1,18 +1,33 @@
 package api;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
+@Epic("Reqres API Tests")
+@Feature("User API")
+@DisplayName("Тесты для пользователей Reqres")
 public class UserTests extends BaseTest {
 
     // Проверка, что пользователь с ID 2 существует и возвращается корректно
     @Test
-    void getUserById() {
+    @Story("Получение информации о пользователе")
+    @Description("Проверка, что пользователь с ID 2 существует и возвращается корректно")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("GET /users/2 — Проверка существующего пользователя")
+   public void getUserById_shouldReturnUser() {
         given()
+                .spec(requestSpec)
                 .when()
                 .get("/users/2")
                 .then()
@@ -20,11 +35,14 @@ public class UserTests extends BaseTest {
                 .body("data.id", equalTo(2));
     }
 
-    // Позитивный тест: успешная авторизация с корректными email и паролем
     @Test
-    void loginWithValidCredentials() {
+    @Story("Авторизация")
+    @Description("Позитивный тест: успешная авторизация с корректными email и паролем")
+    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("POST /login — Успешная авторизация")
+    public void loginWithValidCredentials_shouldReturnToken() {
         given()
-                .header("x-api-key", "reqres-free-v1")
+                .spec(requestSpec)
                 .contentType(ContentType.JSON)
                 .body("{\"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\"}")
                 .when()
@@ -34,11 +52,14 @@ public class UserTests extends BaseTest {
                 .body("token", notNullValue());
     }
 
-    // Негативный тест: попытка авторизации без пароля должна вернуть ошибку 400
     @Test
-    void loginWithoutPassword_shouldReturn400() {
+    @Story("Авторизация")
+    @Description("Негативный тест: попытка авторизации без пароля должна вернуть ошибку 400")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("POST /login — Без пароля (ошибка 400)")
+    public void loginWithoutPassword_shouldReturnError() {
         given()
-                .header("x-api-key", "reqres-free-v1")
+                .spec(requestSpec)
                 .contentType(ContentType.JSON)
                 .body("{\"email\": \"eve.holt@reqres.in\"}") // без password
                 .when()
@@ -48,11 +69,14 @@ public class UserTests extends BaseTest {
                 .body("error", notNullValue());
     }
 
-    // Позитивный тест: создание нового пользователя и проверка возвращаемых данных
     @Test
-    void createUser() {
+    @Story("Создание пользователя")
+    @Description("Позитивный тест: создание нового пользователя и проверка возвращаемых данных")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("POST /users — Создание пользователя")
+    public void createUser_shouldReturnUserId() {
         given()
-                .header("x-api-key", "reqres-free-v1")
+                .spec(requestSpec)
                 .contentType(ContentType.JSON)
                 .body("{\"name\": \"Morpheus\", \"job\": \"leader\"}")
                 .when()
@@ -66,9 +90,13 @@ public class UserTests extends BaseTest {
 
     // Удаление пользователя с ID 2. API должно вернуть 204 No Content
     @Test
-    void deleteUser() {
+    @Story("Удаление пользователя")
+    @Description("Удаление пользователя с ID 2. API должно вернуть 204 No Content")
+    @Severity(SeverityLevel.MINOR)
+    @DisplayName("DELETE /users/2 — Удаление пользователя")
+    public void deleteUser_shouldReturn204() {
         given()
-                .header("x-api-key", "reqres-free-v1")
+                .spec(requestSpec)
                 .when()
                 .delete("/users/2")
                 .then()
